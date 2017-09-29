df77d5e0cb07163ecdaaaf4fdd5e0b31bedd4368
Shanmughasunder V <sxv3899@global.iff.com>
2017-09-29 21:33:48 +0530

    First commit for the docker project

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.listen(8888,function(){
+	console.log('App listening at port 8888');
+	console.log('Hi shanmughasunder');
+	console.log('Now its working I guess');
+})
+console.log(213)
